1f1cfa3edb916aa0c09a6e9c3e41b9226e465119
kunalrathorelnct <kunalrathore.it@gmail.com>
2020-08-18 19:06:18 +0530

    Updates

---
+++ precache-manifest.63ba6f59f2e801d5a925079748361423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f39d85e5abe1cfb3493290d4e41f98",
+    "url": "/covid19live/index.html"
+  },
+  {
+    "revision": "8e81daafccbc09397a5b",
+    "url": "/covid19live/static/css/main.69a1bd65.chunk.css"
+  },
+  {
+    "revision": "e15ed1e9fafd3bb0c2ff",
+    "url": "/covid19live/static/js/2.9b2fb852.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19live/static/js/2.9b2fb852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e81daafccbc09397a5b",
+    "url": "/covid19live/static/js/main.e0397ac7.chunk.js"
+  },
+  {
+    "revision": "da7f45dda9b0798094f8",
+    "url": "/covid19live/static/js/runtime-main.b8f2e19a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19live/static/media/image.d7265326.png"
+  }
+]);
